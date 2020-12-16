5cb55a806a9ff1f2339e440cb7bd135c04edddd1
Rafic El Kouche <raficelkouche@hotmail.com>
2020-12-16 18:26:30 -0500

    add a function to render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
               <img src=${tweetData.user.avatars} alt="">
               <p>${tweetData.user.name}</p>
             </div>
-            <p id="handle">@${tweetData.user.handle}</p>
+            <p id="handle">${tweetData.user.handle}</p>
           </header>
           <div class="tweet">
             <p>${tweetData.content.text}</p>
@@ -28,23 +28,42 @@ $(document).ready(function() {
     return $tweet;
   }
 
-
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+  const renderTweets = function(tweets) {
+    tweets.forEach((elm) => {
+      let $tweet = createTweetElement(elm);
+      $('.tweet-container').append($tweet)
+    })
   }
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-  const $tweet = createTweetElement(tweetData);
+  //const $tweet = createTweetElement(tweetData);
 
   // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-})
+  //console.log($tweet); // to see what it looks like
+  renderTweets(data) // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
