585c670c03b68de9430e10611b8efea99f1d41ef
Rafic El Kouche <raficelkouche@hotmail.com>
2020-12-16 19:51:36 -0500

    add a function to load all the tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,48 +33,26 @@ $(document).ready(function() {
       let $tweet = createTweetElement(elm);
       $('.tweet-container').append($tweet)
     })
-  }
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  };
 
-  // Test / driver code (temporary)
-  renderTweets(data) // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: "http://localhost:8080/tweets"
+    })
+      .then(renderTweets)
+  };
 
+  loadTweets();
+  
   $(".new-tweet form").on("submit", function(event) {
-    console.log($(this).children("#tweet-text").serialize());
     event.preventDefault();
     $.ajax({
       method: "POST",
       url: "http://localhost:8080/tweets",
       data: $(this).children("#tweet-text").serialize()
     })
-      .then((result) => {
-        console.log(result);
-      })
+      .then()
       .catch((result) => {
         alert(result.responseJSON.error)
       })
