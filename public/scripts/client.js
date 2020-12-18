31b4115e4feb44d73ee5ee27e0098a787bbe746c
Rafic El Kouche <raficelkouche@hotmail.com>
2020-12-18 16:15:36 -0500

    add function to validate tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     div.text(input);
     return div.text();
   }
+
   //Determine how long ago a tweet was posted
   const adjustTweetTime = function (timeInMs) {
     const MS_ONE_DAY = 86400000;
@@ -14,12 +15,12 @@ $(document).ready(function() {
 
     if (difference >= MS_ONE_DAY) {
       //report in days if difference is more than 24 hours
-      return `${Math.floor(difference / MS_ONE_DAY)} day(s) ago`
+      return `${Math.floor(difference / MS_ONE_DAY)} day(s) ago`;
       //report in minutes if difference is less than 24 hours
     } else if (difference < MS_ONE_DAY && difference >= MS_ONE_MIN) {
-      return `${Math.floor(difference / MS_ONE_MIN)} minute(s) ago`
+      return `${Math.floor(difference / MS_ONE_MIN)} minute(s) ago`;
     } else {
-      return `<1 minute ago`
+      return `<1 minute ago`;
     }
   }
 
@@ -48,6 +49,7 @@ $(document).ready(function() {
     return $tweet;
   }
 
+  //Render the tweets on the page ordered by date (most recent first)
   const renderTweets = function(tweets) {
     tweets.forEach((elm) => {
       let $tweet = createTweetElement(elm);
@@ -55,6 +57,7 @@ $(document).ready(function() {
     })
   };
 
+  //load all the tweets and render them on the page
   const loadTweets = function() {
     $.ajax({
       method: 'GET',
@@ -63,6 +66,7 @@ $(document).ready(function() {
       .then(renderTweets)
   };
 
+  //Get the most recent tweet only and render it on the page
   const updateTweets = function() {
     $.ajax({
       method: 'GET',
@@ -81,24 +85,33 @@ $(document).ready(function() {
     $form.find("output").text(140);
   }; 
 
+  //Validate the tweet before posting
+  const validateTweet = function($data, $error) {
+    if (!$data.val()) {
+      $error.children("p").text("Tweet is empty! I am sure you can come up with something better than that...")
+      $error.slideDown();
+      return false;
+    } else if ($data.val().length > 140) {
+      $error.children("p").text("That was a pretty long tweet! You might want to cut it down a bit to see it come to life...")
+      $error.slideDown();
+      return false;
+    } else {
+      return true;
+    }
+  }
   
   //New tweet form validation and submission
   $(".new-tweet form").on("submit", function(event) {
     event.preventDefault();
+
     const $data = $(this).children("#tweet-text");
     const $error = $(".new-tweet .error-message");
-    
+
     //hide the error on submit
     $error.slideUp();
 
-    if (!$data.val()) {
-      $error.children("p").text("Tweet is empty! I am sure you can come up with something better than that...")
-      $error.slideDown();
-    } else if ($data.val().length > 140) {
-      //create an error function that takes in the element and the type of error
-      $error.children("p").text("That was a pretty long tweet! You might want to cut it down a bit to see it come to life...")
-      $error.slideDown();
-    } else {
+    //Post the tweet if it is valid
+    if (validateTweet($data, $error)) {
         $.ajax({
           method: "POST",
           url: "http://localhost:8080/tweets",
@@ -112,27 +125,32 @@ $(document).ready(function() {
             alert(result.responseJSON.error)
           })
     }
-  })
+  });
+
+  //Hide the error messages and new tweet form when the document loads
   $(".new-tweet .error-message").hide();
   $(".new-tweet").children("form").hide();
 
+  //registers a handler for the arrows in the navbar
   $(".fa-angle-double-down").on("click", function () {
-    $(".error-message").slideUp();
+    $(".error-message").slideUp(); //hide the error in case it was there
+    //slides the form up/down depending on its current status
     $(".new-tweet").children("form").slideToggle('slow', function () {
       $("#tweet-text").focus();
-      
     })
   })
 
+  //Displays an arrow at the bottom of the page when the user starts scrolling
   $(window).on("scroll", function () {
     $(".lower-toggle-button").css("display", "flex");
-
   })
+    //Scroll to the top of the page and show the form when the user clicks on the arrows
+    $(".lower-toggle-button").on("click", function () {
+      $("html").animate({ scrollTop: 0 }, "slow")
+      $(".new-tweet").children("form").slideDown();
+      $("#tweet-text").focus();
+    })
 
-  $(".lower-toggle-button").on("click", function () {
-    $("html").animate({ scrollTop: 0 }, "slow")
-    $(".new-tweet").children("form").slideDown();
-    $("#tweet-text").focus();
-  })
+  //Load the tweets when the document is ready
   loadTweets();
 });
